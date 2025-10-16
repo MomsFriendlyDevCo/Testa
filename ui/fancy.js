dede82fc59fb25fefd56efd40dea9b4b62a9864c
Matt Carter <m@ttcarter.com>
2025-10-16 10:21:59 +1000

    refactor: Fancy UI output can now report on (one line of) stages, logs and warns

--- ui/fancy.js
+++ ui/fancy.js
@@ -12,6 +12,9 @@ export default async function TestaUIFancy({TestaBase}) {
 	let listr; // Eventual Listr instance, created when onTests() resolves
 	let failed = []; // Eventual array of failed tests
 
+	// Map for TestaTest -> ListrTasks, allocated when setting up the main Listr instance
+	let test2task = new Map();
+
 	return Promise.resolve()
 		.then(()=> TestaBase.execAll({
 			onTests: tests => { // Run all queued tests
@@ -25,7 +28,7 @@ export default async function TestaUIFancy({TestaBase}) {
 					tests.map(test => ({
 						title: test.toString(),
 						task: (ctx, task) => {
-							task.listrTask = task;
+							test2task.set(test, task);
 							return test.resolvers.promise;
 						},
 					})),
@@ -37,6 +40,15 @@ export default async function TestaUIFancy({TestaBase}) {
 
 				listr.run();
 			},
+			onTestLog: (test, msg) => {
+				test2task.get(test).output = msg.join(' ');
+			},
+			onTestWarn: (test, msg) => {
+				test2task.get(test).output = msg.join(' ');
+			},
+			onTestStage: (test, msg) => {
+				test2task.get(test).output = msg.join(' ');
+			},
 			onTestRejected: (test, err) => {
 				test.resolvers.resolve(err);
 				failed.push(test);
@@ -44,8 +56,12 @@ export default async function TestaUIFancy({TestaBase}) {
 			onTestResolved: test => {
 				test.resolvers.resolve();
 			},
+			onTestTimeout: test => {
+				test.resolvers.reject('Timeout');
+				failed.push(test);
+			},
 			onTestSkipped: (test, msg) => {
-				test.listrTask.skip(msg);
+				test2task.get(test).skip(msg);
 			},
 		}))
 		.then(()=> UIReport({TestaBase, failed}))
